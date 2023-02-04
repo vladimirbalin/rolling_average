a4645bed8817691aed70ec39fe3af0f3d17f12cb
vladimirbalin <vladimirzq@Gmail.com>
2023-02-04 13:26:55 +0700

    Cleanup

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,7 +3,6 @@ import {createTable} from "./functions.js";
 
 document.addEventListener('DOMContentLoaded', function () {
     const $app = document.getElementById('app');
-
     let linksContainer = document.querySelector('.links');
     let links = document.querySelectorAll('a');
 
@@ -14,18 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        el.addEventListener('click', async function (event) {
+        el.addEventListener('click', listener);
+
+        async function listener(event) {
             event.preventDefault();
 
             try {
                 const endpoint = config.endpoint + el.getAttribute('data-href');
                 const {data: json} = await axios.get(endpoint);
-
                 const table = createTable(json);
+
                 $app.replaceChildren(table);
             } catch (e) {
                 console.log(e)
             }
-        })
+        }
     }
 })
